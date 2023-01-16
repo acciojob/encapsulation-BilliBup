287371f2f354b352bb018f66d1906d1fb23a1932
SHRISHTY MALVIYYA <malviya.shrishty@gmail.com>
2023-01-16 13:05:17 +0530

    acciojob assignment 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String []args){
+      RWOnly obj = new RWOnly();
+      // obj.name Error: make package private or name a protected or public
+
+      String gotname = "Shrishty";
+      obj.setName(gotname);
+      System.out.println(obj.getName());
+  }
 }
